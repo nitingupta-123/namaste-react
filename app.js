423634957b63e0fa7bb2d105c2e42f5d410d8e31
NITIN GUPTA <nitin.gupta@xecurify.com>
2024-01-14 17:55:58 +0530

    episode 2: igniting our app

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
  *  </div>
  * </div>
  */
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
 
+console.log(ReactDOM) 
 const parent = React.createElement("div", { id: 'parent' },
     React.createElement("div", { id: 'child' },
         [React.createElement("h1", { id: "heading" }, "I am heading tag"),
